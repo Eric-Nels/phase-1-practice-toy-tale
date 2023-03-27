75fe6ae0a6d0b138ecbd99f618fed9566ca0e2b2
Eric Nelson <ernelson12o0@gmail.com>
2023-03-26 20:58:41 -0400

    Lab Complete

--- src/index.js
+++ src/index.js
@@ -13,3 +13,22 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+const servUrl = 'http://localhost:3000/toys';
+fetch(servUrl)
+  .then(response => response.json())
+  .then(data => {
+    const myDiv = document.querySelector('.card');
+    data.forEach(obj => {
+      const objDiv = document.createElement('div');
+      objDiv.innerHTML = `
+        <h2>${obj.name}</h2>
+        <p>${obj.description}</p>
+        <img src="${obj.image}">
+      `;
+      myDiv.appendChild(objDiv);
+    });
+  })
+  .catch(error => console.error(error));
+})
