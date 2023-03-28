35be9da9e63ba4ab0bbdc53a92fc5106e6a3ed8a
Eric Nelson <ernelson12o0@gmail.com>
2023-03-28 15:23:21 -0400

    Lab COmplete

--- src/index.js
+++ src/index.js
@@ -14,21 +14,36 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-document.addEventListener('DOMContentLoaded', () => {
 const servUrl = 'http://localhost:3000/toys';
-fetch(servUrl)
-  .then(response => response.json())
-  .then(data => {
-    const myDiv = document.querySelector('.card');
-    data.forEach(obj => {
-      const objDiv = document.createElement('div');
-      objDiv.innerHTML = `
-        <h2>${obj.name}</h2>
-        <p>${obj.description}</p>
-        <img src="${obj.image}">
-      `;
-      myDiv.appendChild(objDiv);
-    });
-  })
-  .catch(error => console.error(error));
-})
+const toyCollection = document.getElementById('toy-collection');
+document.addEventListener('DOMContentLoaded', () => {
+  fetch(servUrl)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(obj => {
+        const div = document.createElement('div');
+        div.className = 'card';
+        createCard(obj, div)
+        toyCollection.appendChild(div);
+      });
+    })
+    .catch(error => console.error(error));
+});
+
+function createCard(data, parent) {
+  const h2 = document.createElement('h2');
+  h2.textContent = data.name;
+  const img = document.createElement('img');
+  img.src = data.image;
+  img.className = 'toy-avatar';
+  const p = document.createElement('p');
+  p.textContent = data.likes;
+  const button = document.createElement('button');
+  button.className = 'like-btn';
+  button.id = data.id;
+
+  parent.appendChild(h2)
+  parent.appendChild(img)
+  parent.appendChild(p)
+  parent.appendChild(button)
+}
